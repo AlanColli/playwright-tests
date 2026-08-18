5471e4127a5976945f77966edb82d1b3b81779bd
alancollinetti <alancollinetti@gmail.com>
2026-08-19 00:32:59 +0100

    correcao de fixture

--- Fixtures/auth.fixture.ts
+++ Fixtures/auth.fixture.ts
@@ -10,7 +10,7 @@ type MyFixtures = {
 // 2. CRIA O TEST COM O TIPO DECLARADO
 export const test = base.extend<MyFixtures>({
   loggedInPage: async ({ page }: { page: Page }, use) => {
-    // 👆 ADICIONEI O TIPO AQUI
+    // ADICIONEI O TIPO AQUI
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await loginPage.login('standard_user', 'secret_sauce');
